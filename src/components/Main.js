d3ba903ad0d850381da9eaa2385c31f32ea7a45d
NgocDuong <baongocduong1@gmail.com>
2021-10-03 12:45:05 +0700

    gameinfo for user

---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+    return (
+        <div className='container'>
+             <h1>Obstacles Crossed</h1>
+             <ul>
+                 <li>
+                     Genre : Platform Game
+                 </li>
+                 <li>
+                 Publisher : Best Team Name
+                 </li>
+                 <li>
+                 Issue Date : ? / ? /2021
+                 </li>
+                 <li>
+                     Age : 6 +
+                 </li>
+             </ul>
+             
+             </div>   
+    )
+}
+
+export default Main
---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '../Main';
+import Footer from '../Footer';
+function AboutUs() {
+    return (
+        <>
+         <Main/>
+         <Footer/>
+        </>
+    )
+}
+
+export default AboutUs
